fa4331dd302b6ad4d683b2a8fcfd123c8eaec1ed
Bruce Kristelijn <bruce.kristelijn@gmail.com>
2026-04-23 10:55:17 +0200

    Improvements to the col classes inside the image gallery section

--- web/src/components/sections/ImageGallerySection.vue
+++ web/src/components/sections/ImageGallerySection.vue
@@ -1,28 +1,24 @@
 <script setup>
-import SimpleCard from '../SimpleCard.vue'
-defineProps(['section'])
+  import SimpleCard from '../SimpleCard.vue'
+  import { sectionImgAttrs } from '../../sanity/imageUrl.js'
+  defineProps(['section'])
 </script>
 
 <template>
   <SimpleCard :title="section.title">
     <p v-if="section.body" class="mb-4 opacity-80 text-sm">{{ section.body }}</p>
-    <div
-      class="gap-10 p-5"
-      :class="{
-        'grid grid-cols-1':                           section.layout === 'single',
-        'grid grid-cols-2':                           section.layout === '2col',
-        'grid grid-cols-2 md:grid-cols-3':            section.layout === '3col',
-        'grid grid-cols-3 md:grid-cols-5':            section.layout === '5col',
-        'grid grid-cols-2 md:grid-cols-3':            !section.layout || section.layout === 'auto',
-      }"
-    >
-      <figure v-for="img in section.images" :key="img.url" class="m-0">
-        <img
-          :src="img.url + '?auto=format&w=800'"
-          :alt="img.caption ?? ''"
-          class="w-full rounded-md block"
-          loading="lazy"
-        />
+    <div class="gap-10 p-5" :class="[
+      'gap-10 p-5 grid',
+      {
+        'grid-cols-1': section.layout === 'single',
+        'grid-cols-2': section.layout === '2col',
+        'grid-cols-2 md:grid-cols-3': section.layout === '3col' || !section.layout || section.layout === 'auto',
+        'grid-cols-3 md:grid-cols-5': section.layout === '5col',
+      }
+    ]">
+      <figure v-for="img in section.images" :key="img._ref" class="m-0">
+        <img v-bind="sectionImgAttrs(img, [600, 1000, 1600])" :alt="img.caption ?? ''" class="w-full block"
+          :class="{ 'rounded-md': section.rounded === true }" loading="lazy" />
         <figcaption v-if="img.caption" class="text-xs mt-1 opacity-60 text-center">
           {{ img.caption }}
         </figcaption>
